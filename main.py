f0727eab9d2a9d906d230162a2aa8fdac060ac7f
KrouZ_CZ <64690903+KvantPro@users.noreply.github.com>
2022-03-05 20:06:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import socket
 import json
 import os
+from sys import exit
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cls = lambda: os.system('cls')
-client.connect(('127.0.0.1', 2000))
-client.send('FindGame'.encode('utf-8'))
-print('Find apponent')
-def polle(pole):
+os.system('mode 25,15')
+try:
+    client.connect(('127.0.0.1', 2000))
+except:
+    print("Server closed")
+    exit()
+def polle(pole, f):
     cls()
     for i, item in enumerate(pole):
         match item:
-            case 0: print('* ', end='')
-            case 1: print('x ', end='')
-            case 2: print('o ', end='')
+            case 0: print(polee(i), end='')
+            case 1: print('X ', end='')
+            case 2: print('O ', end='')
+        if i == 5: print('You is X' if wy == '1' else 'You is O', end='')
         if (i + 1) % 3 == 0 and i != 1: print('\n')
 def res(a):
     match a:
@@ -25,6 +30,23 @@ def res(a):
         case '7': return "0"
         case '8': return "1"
         case '9': return "2"
+    return '9'
+def polee(i):
+    match i:
+        case 0: return '7 '
+        case 1: return '8 '
+        case 2: return '9 '
+        case 3: return '4 '
+        case 4: return '5 '
+        case 5: return '6 '
+        case 6: return '1 '
+        case 7: return '2 '
+        case 8: return '3 '
+    return '9'
+client.send('FindGame'.encode('utf-8'))
+print('Find apponent')
+wy = None
+wy = client.recv(1024).decode('utf-8')
 while True:
     move = client.recv(1024).decode('utf-8')
     if move == 'exit':
@@ -36,7 +58,7 @@ while True:
         print('You loose')
         break
     pole = json.loads(client.recv(1024).decode('utf-8'))
-    polle(pole)
+    polle(pole, move)
     if move == '1':
         a = input("> ")
         client.send(res(a).encode('utf-8'))
